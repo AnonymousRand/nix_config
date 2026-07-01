d820481173d24d8f95a90592b514eecb4b827f19
AnonymousRand <anonymouserand@gmail.com>
2026-07-01 12:11:14 -0700

    uh oh versioning issues?

--- modules/users/meow/home_manager/niri.nix
+++ modules/users/meow/home_manager/niri.nix
@@ -1,11 +1,12 @@
 { inputs, ... }: {
-  flake.homeModules.meow = { config, ... }: {
+  flake.homeModules.meow = { config, pkgs, ... }: {
     imports = [
       inputs.niri.homeModules.niri
     ];
 
     programs.niri = {
       enable = true;
+      package = pkgs.niri-unstable;
       config = builtins.readFile ./dotfiles/niri_config.kdl;
     };
   };
