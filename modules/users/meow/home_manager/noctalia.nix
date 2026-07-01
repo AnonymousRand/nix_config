b3f5524b33237843dc85d7851d9eacb4820cb154
AnonymousRand <anonymouserand@gmail.com>
2026-07-01 16:30:58 -0700

    trying mkForce

--- modules/users/meow/home_manager/noctalia.nix
+++ modules/users/meow/home_manager/noctalia.nix
@@ -1,15 +1,15 @@
 { inputs, ... }: {
-  flake.homeModules.meow = {
+  flake.homeModules.meow = { lib, ... }: {
     imports = [
       inputs.noctalia.homeModules.default
     ];
 
     programs.noctalia-shell = {
       enable = true;
-      settings.colorSchemes.predefinedScheme = "Catppuccin";
-      #settings = {
-      #  colorSchemes.predefinedScheme = "Catppuccin";
-      #};
+      #settings.colorSchemes.predefinedScheme = "Catppuccin";
+      settings = lib.mkForce {
+        colorSchemes.predefinedScheme = "Catppuccin";
+      };
     };
   };
 }
