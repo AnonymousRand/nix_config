98a4ece1387e2268cafe76227f4aa0d4dc819129
AnonymousRand <anonymouserand@gmail.com>
2026-07-03 22:49:45 -0700

    forgot to add clang and gcc to cpp shell...

--- shells/cpp.nix
+++ shells/cpp.nix
@@ -2,7 +2,9 @@
   perSystem = { pkgs, self', ... }: {
     devShells.cpp = pkgs.mkShell {
       packages = with pkgs; [
+        clang
         cmake
+        gcc
         gnumake
         just
         meson
