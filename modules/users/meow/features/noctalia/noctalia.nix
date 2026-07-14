8d95a6422c48a5d98dd9d383c6f4cecd9acd5284
AnonymousRand <anonymouserand@gmail.com>
2026-07-13 23:35:41 -0700

    fix a bug (although there are bugs galore)

--- modules/users/meow/features/noctalia/noctalia.nix
+++ modules/users/meow/features/noctalia/noctalia.nix
@@ -1,33 +1,37 @@
 { inputs, ... }: {
-  flake.homeModules.meow = { my, ... }: {
+  flake.homeModules.meow = { my, lib, ... }: {
     imports = [
       inputs.noctalia.homeModules.default
     ];
 
     programs.noctalia = {
       enable = true;
-      settings = my.theme.noctaliaCustomColors // {
-        theme = {
-          # declare my custom color palette for Noctalia app theming
-          source = "custom";
-          custom_palette = "anonymousrand";
+      settings = lib.mkMerge [
+        # load custom colors
+        my.theme.noctaliaCustomColors
+        {
+          theme = {
+            # declare my custom color palette for Noctalia app theming
+            source = "custom";
+            custom_palette = "anonymousrand";
 
-          # switch between light and dark themes automatically
-          mode = "auto";
+            # switch between light and dark themes automatically
+            mode = "auto";
 
-          templates = {
-            # enable built-in app theming, which uses templates to map
-            # Noctalia color palette colors to each app's configs
-            enable_builtin_templates = true;
+            templates = {
+              # enable built-in app theming, which uses templates to map
+              # Noctalia color palette colors to each app's configs
+              enable_builtin_templates = true;
+            };
           };
-        };
 
-        location = {
-          auto_locate = false;
-          sunrise = "09:00";
-          sunset = "18:00";
-        };
-      };
+          location = {
+            auto_locate = false;
+            sunrise = "09:00";
+            sunset = "18:00";
+          };
+        }
+      ];
     };
 
     xdg.configFile."noctalia/palettes/anonymousrand.json".text = builtins.toJSON my.theme.colors.m3Palette;
