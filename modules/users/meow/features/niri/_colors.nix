8aa8b899f641d2bffca0473d76b3db39ee5b5d91
AnonymousRand <anonymouserand@gmail.com>
2026-07-17 01:04:02 -0700

    ugh

--- modules/users/meow/features/niri/_colors.nix
+++ modules/users/meow/features/niri/_colors.nix
@@ -1,12 +1,12 @@
 {
   # for active window border/focus ring gradient
   niri_active_gradient_start = {
-    color_light = "#ff409f";
-    color_dark  = "#ff409a";
+    color_light = "#ff7abd";
+    color_dark  = "#ff4da6";
   };
 
   niri_active_gradient_end = {
-    color_light = "#00e1ff";
+    color_light = "#40efff";
     color_dark  = "#00e1ff";
   };
 }
