f98de72c68f951aee8acc210941c9d7d5ce7cc75
AnonymousRand <anonymouserand@gmail.com>
2026-07-17 01:17:37 -0700

    ugh, part 2

--- modules/users/meow/features/niri/_colors.nix
+++ modules/users/meow/features/niri/_colors.nix
@@ -1,12 +1,12 @@
 {
   # for active window border/focus ring gradient
   niri_active_gradient_start = {
-    color_light = "#ff7abd";
-    color_dark  = "#ff4da6";
+    color_light = "#ff75ba";
+    color_dark  = "#ff75ba";
   };
 
   niri_active_gradient_end = {
     color_light = "#40efff";
-    color_dark  = "#00e1ff";
+    color_dark  = "#40efff";
   };
 }
