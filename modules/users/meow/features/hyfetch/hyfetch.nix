259deaa15df594d00f67a9abdea407a9fba661f8
AnonymousRand <anonymouserand@gmail.com>
2026-07-19 01:00:53 -0700

    okie pls dont break

--- modules/users/meow/features/hyfetch/hyfetch.nix
+++ modules/users/meow/features/hyfetch/hyfetch.nix
@@ -1,7 +1,12 @@
 {
-  flake.homeModules.meow = {
+  flake.homeModules.meow = { pkgs, ... }: {
     programs.hyfetch = {
       enable = true;
+      # use my own custom wrapped `hyfetch` which allows different `lightness` settings per mode
+      package = pkgs.callPackage ./lightness_per_mode_wrapper.nix {
+        lightModeLightness = 0.85;
+        darkModeLightness  = 0.58;
+      };
     };
 
     xdg.configFile."hyfetch.json".source = ./dotfiles/hyfetch.json;
