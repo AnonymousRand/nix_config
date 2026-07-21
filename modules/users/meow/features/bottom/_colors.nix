24d2a881c96e25700ecf5479fce98e3396728be8
AnonymousRand <anonymouserand@gmail.com>
2026-07-21 13:45:03 -0700

    bottomer

---
+++ modules/users/meow/features/bottom/_colors.nix
@@ -0,0 +1,26 @@
+{
+  cpu_color_red = {
+    color_light = "#ff6666";
+    color_dark  = "#ff6666";
+  };
+  cpu_color_orange = {
+    color_light = "#ffa600";
+    color_dark  = "#ffa600";
+  };
+  cpu_color_yellow = {
+    color_light = "#f7ce00";
+    color_dark  = "#ffea00";
+  };
+  cpu_color_green = {
+    color_light = "#76d600";
+    color_dark  = "#8cff00";
+  };
+  cpu_color_blue = {
+    color_light = "#00eaff";
+    color_dark  = "#00eaff";
+  };
+  cpu_color_purple = {
+    color_light = "#ff99f7";
+    color_dark  = "#ffa6f8";
+  };
+}
