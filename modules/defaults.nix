6dbbbe864a10476ddcd4d859fd2eb1f42a0a2239
AnonymousRand <anonymouserand@gmail.com>
2026-07-25 19:53:44 -0700

    regeorg

--- modules/defaults.nix
+++
@@ -1,7 +0,0 @@
-{
-  den.reservedKeys = [
-    # reserve `settings` key so settings generator in `_settings_generator.nix` can work
-    # without den thinking that `settings` is one of their special aspect keys
-    "settings"
-  ];
-}
