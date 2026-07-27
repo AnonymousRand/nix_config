0ba7427d9d209f0304297a777e7b3a134a8d791c
AnonymousRand <anonymouserand@gmail.com>
2026-07-26 23:09:34 -0700

    tweak some keybinds

--- modules/users/meow/features/terminal/fish/dotfiles/functions/fish_user_key_bindings.fish
+++ modules/users/meow/features/terminal/fish/dotfiles/functions/fish_user_key_bindings.fish
@@ -1,5 +1,5 @@
 function fish_user_key_bindings
-    # remove `ctrl + x` and `ctrl + v` copy/paste keybinds, but leave them to the terminal emulator
+    # remove ctrl+x and ctrl+v copy/paste keybinds, but leave them to the terminal emulator
     # except for copying in visual mode, which ghostty at least doesn't seem to be able to do
     bind --erase --preset -M insert  ctrl-x
     bind --erase --preset -M visual  ctrl-x
@@ -8,15 +8,15 @@ function fish_user_key_bindings
     bind --erase --preset -M visual  ctrl-v
     bind -M visual ctrl-shift-c fish_clipboard_copy
 
-    # enable `ctrl + r` and `ctrl + s` history pager in vim insert mode still
+    # enable ctrl+r and ctrl+s history pager in vim insert mode still
     bind -M insert ctrl-r history-pager
     bind -M insert ctrl-s pager-toggle-search
 
-    # enable `ctrl + backspace` to delete word and `alt + backspace` to delete argument in vim insert mode
+    # enable ctrl+backspace to delete word and alt+backspace to delete argument in vim insert mode
     bind -M insert ctrl-backspace "if fish_in_macos_terminal; commandline -f backward-kill-token; else commandline -f backward-kill-word; end"
     bind -M insert alt-backspace  "if fish_in_macos_terminal; commandline -f backward-kill-word; else commandline -f backward-kill-token; end"
 
-    # enable `alt + ,` to search forward in previous arguments (`alt + .` should be default, but just in case :3
+    # enable alt+, to search forward in previous arguments (alt+. should be default, but just in case :3
     bind -M insert alt-. history-token-search-backward
     bind -M visual alt-. history-token-search-backward
     bind -M insert alt-comma  history-token-search-forward
