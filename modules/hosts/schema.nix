906fd9d1f204bcd4c47f7b3510ca3ee37fc6c0a5
AnonymousRand <anonymouserand@gmail.com>
2026-07-26 21:58:21 -0700

    add comment

--- modules/hosts/schema.nix
+++ modules/hosts/schema.nix
@@ -34,6 +34,7 @@
 
     config = {
       # change default aspect name associated with host entities to fit our naming scheme
+      # (note that this requires the target aspect to exist *first*)
       aspect = den.aspects.hosts.${host.name};
     };
   };
