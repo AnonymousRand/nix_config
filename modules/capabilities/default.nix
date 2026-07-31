ec10e58576448aaf9b774060a898f9ed3696158a
AnonymousRand <anonymouserand@gmail.com>
2026-07-30 19:22:07 -0700

    so this proof of concept works

--- modules/capabilities/default.nix
+++ modules/capabilities/default.nix
@@ -2,15 +2,25 @@
   # TODO: if this works, try moving this to a schema's includes or something
   # that prob won't work though; in that case have to manually iterate through den.aspects.hosts
   # and den.aspects.users maybe?
-  den.aspects.hosts.desktop = { host, ... }: {
-    __functor = self: ctx: {
-      # for each aspect in self.includes
-      # for each capability in aspect.meta.requiredCapabilities
-      # if capability in host.capability, add to includes, else add nothing (lib.optional)
-      includes = builtins.filter (aspect:
-        builtins.foldl' (acc: new: acc && host.capabilities.${new}.supported)
-          true (aspect.meta.requiredCapabilities or [])
-      ) self.includes;
-    };
-  };
+  #den.aspects.hosts.desktop = { host, ... }: {
+  #  __functor = self: ctx: {
+  #    includes = builtins.filter (aspect:
+  #      builtins.foldl' (acc: new: acc && host.capabilities.${new}.supported)
+  #        true (aspect.meta.requiredCapabilities or [])
+  #    ) self.includes;
+  #  };
+  #};
+  #den.aspects.features.tools.brightnessctl = { host, config, lib, ... }: {
+  #  __functor = self: ctx:
+  #    let
+  #      #areRequiredCapabilitiesMet =
+  #      #  builtins.foldl' (acc: new: acc && host.capabilities.${new}.supported)
+  #      #  true config.meta.requiredCapabilities;
+  #      areRequiredCapabilitiesMet = false;
+  #    in
+  #    {
+  #      nixos = lib.mkIf areRequiredCapabilitiesMet self.nixos;
+  #      homeManager = lib.mkIf areRequiredCapabilitiesMet self.homeManager;
+  #    };
+  #};
 }
--- modules/features/tools/brightnessctl.nix
+++ modules/features/tools/brightnessctl.nix
@@ -1,11 +1,24 @@
 {
-  den.aspects.features.tools.brightnessctl = {
+  den.aspects.features.tools.brightnessctl = { config, lib, ... }: {
     meta.requiredCapabilities = [ "brightness" ];
 
-    homeManager = { pkgs, ... }: {
-      home.packages = [
-        pkgs.brightnessctl
-      ];
-    };
+    #homeManager = { pkgs, ... }: {
+    #  home.packages = [
+    #    pkgs.brightnessctl
+    #  ];
+    #};
+
+    homeManager = { host, pkgs, ... }:
+      let
+        #areRequiredCapabilitiesMet = false;
+        areRequiredCapabilitiesMet =
+          builtins.foldl' (acc: new: acc && host.capabilities.${new}.supported)
+          true config.meta.requiredCapabilities;
+      in
+      lib.mkIf areRequiredCapabilitiesMet {
+        home.packages = [
+          pkgs.brightnessctl
+        ];
+      };
   };
 }
