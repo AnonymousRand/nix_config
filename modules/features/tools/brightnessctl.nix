e5c01d7e3c1c714643d8f3569ea0568338a9814e
AnonymousRand <anonymouserand@gmail.com>
2026-07-31 21:22:25 -0700

    unsuccessfully tried some things :/

--- modules/features/tools/brightnessctl.nix
+++ modules/features/tools/brightnessctl.nix
@@ -1,9 +1,19 @@
-{
-  den.aspects.features.tools.brightnessctl = { config, lib, ... }: {
+{ den, ... }: {
+  den.aspects.features.tools.brightnessctl = {
     homeManager = { host, pkgs, ... }: import ../_require_capabilities.nix host [ "brightness" ] {
       home.packages = [
         pkgs.brightnessctl
       ];
     };
+
+    #includes = [
+    #  den.aspects.capabilities.require-capabilities
+    #];
+    #requiredCapabilities = [ "graphics" ];
+    #rawHomeManager = { pkgs, ... }: {
+    #  home.packages = [
+    #    pkgs.brightnessctl
+    #  ];
+    #};
   };
 }
