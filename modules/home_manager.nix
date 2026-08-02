c7e4d3cb29b738f115b603881a0aec54a5ccab9f
AnonymousRand <anonymouserand@gmail.com>
2026-08-01 23:44:34 -0700

    add comment

--- modules/home_manager.nix
+++ modules/home_manager.nix
@@ -8,7 +8,8 @@
   };
 
   imports = [
-    # integration of home manager for flake-parts; doesn't seem to be needed for den?
+    # integration of home manager for flake-parts; doesn't seem to be needed for den? (i think
+    # it exposes `flake.homeModules` and `flake.homeConfigurations`, neither of which den uses)
     #inputs.home-manager.flakeModules.home-manager
   ];
 }
