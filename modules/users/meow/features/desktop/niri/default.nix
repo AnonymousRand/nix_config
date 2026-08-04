d9a3f3e3de4345877b5e8482a8828393646a8b48
AnonymousRand <anonymouserand@gmail.com>
2026-08-03 22:52:47 -0700

    noctalia and niri tweaks

--- modules/users/meow/features/desktop/niri/default.nix
+++ modules/users/meow/features/desktop/niri/default.nix
@@ -7,18 +7,29 @@
     homeManager = { config, ... }:
       let
         noctaliaThemeCfgPath = "niri/noctalia_theme.kdl";
+        cfgFiles = builtins.filter
+          (filename: filename != "noctalia_theme.kdl")
+          (builtins.attrNames (builtins.readDir ./dotfiles));
       in
       {
         wayland.windowManager.niri = {
-          extraConfig = ''
-            ${builtins.readFile ./dotfiles/config.kdl}
-
-            // put at end as `include`s override prior options
-            // optional include to pass `niri validate` when home manager is building,
-            // as then `noctalia.kdl` doesn't exist yet
-            include optional=true "${config.xdg.configHome}/${noctaliaThemeCfgPath}"
-          '';
+          extraConfig =
+            # (optional include to pass `niri validate` when home manager is building,
+            # as then the `xdg.configFile` and noctalia templates might not exist yet)
+            builtins.foldl' (acc: new:
+              acc + "\ninclude optional=true \"${config.xdg.configHome}/niri/${new}\""
+            ) "" cfgFiles
+            + "\ninclude optional=true \"${config.xdg.configHome}/${noctaliaThemeCfgPath}\"";
         };
+
+        # we need to do this insitead of a recursive `xdg.configFile` to specifically exclude
+        # the unrendered noctalia theme template file :(
+        xdg.configFile = builtins.listToAttrs (builtins.map (
+          filename: {
+            name = "niri/${filename}";
+            value = { source = ./dotfiles/${filename}; };
+          }
+        ) cfgFiles);
 
         # noctalia theming
         utils.noctalia-theming = {
--- modules/users/meow/features/desktop/noctalia/default.nix
+++ modules/users/meow/features/desktop/noctalia/default.nix
@@ -9,53 +9,19 @@
         wallpapersCfgPath = "noctalia/wallpapers";
       in
       {
+        # (note: can't user `config.toml` dotfile as `settings` also set in noctalia theming aspect,
+        # and `settings` seems to completely set the entire `config.toml` file)
+        xdg.configFile."noctalia/config2.toml".source = ./dotfiles/config.toml;
+
         xdg.configFile."${wallpapersCfgPath}" = {
           source = ./dotfiles/wallpapers;
           recursive = true;
         };
 
         programs.noctalia = {
-          # (note: can't move to dotfile as `settings` also set in `den.aspects.utils.noctalia-theming`,
-          # and `settings` seems to completely set the entire config file)
           settings = {
-            theme = {
-              # switch between light and dark themes automatically
-              mode = "auto";
-
-              templates = {
-                enable_builtin_templates = true;
-              };
-            };
-
-            wallpaper = rec {
-              enabled = true;
-              # (note that `directory` only seems to really be for the automation/randomizer)
-              directory = "${config.xdg.configHome}/${wallpapersCfgPath}";
-              default.path = "${directory}/whitepine.png";
-              fill_mode = "fit";
-              transition = [ "fade" ];
-              transition_on_startup = true;
-              transition_duration = 1500;
-
-              automation = {
-                enabled = true;
-                interval_seconds = 1800;
-                order = "random";
-                recursive = false;
-              };
-            };
-
-            location = {
-              auto_locate = false;
-              custom_schedule = true;
-              sunrise = "09:00";
-              sunset = "18:00";
-            };
-
-            shell = {
-              show_location = false;
-              polkit_agent = true;
-            };
+            # (note that `directory` only seems to be for automation/randomizer, else set `default.path`)
+            wallpaper.directory = "${config.xdg.configHome}/${wallpapersCfgPath}";
           };
         };
       };
