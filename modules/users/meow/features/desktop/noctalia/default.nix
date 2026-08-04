ef649ece54743d1c7c542c797a1204a61e2b6d7b
AnonymousRand <anonymouserand@gmail.com>
2026-08-03 20:02:54 -0700

    basic wallpaper (whitepine <3)

--- modules/users/meow/features/desktop/noctalia/default.nix
+++ modules/users/meow/features/desktop/noctalia/default.nix
@@ -4,28 +4,60 @@
       den.aspects.features.desktop.noctalia
     ];
 
-    homeManager = {
-      programs.noctalia = {
-        # (note: can't move to dotfile as `settings` also set in `den.aspects.utils.noctalia-theming`,
-        # and `settings` seems to completely set the entire config file)
-        settings = {
-          theme = {
-            # switch between light and dark themes automatically
-            mode = "auto";
+    homeManager = { config, ... }:
+      let
+        wallpapersCfgPath = "noctalia/wallpapers";
+      in
+      {
+        xdg.configFile."${wallpapersCfgPath}" = {
+          source = ./dotfiles/wallpapers;
+          recursive = true;
+        };
 
-            templates = {
-              enable_builtin_templates = true;
+        programs.noctalia = {
+          # (note: can't move to dotfile as `settings` also set in `den.aspects.utils.noctalia-theming`,
+          # and `settings` seems to completely set the entire config file)
+          settings = {
+            theme = {
+              # switch between light and dark themes automatically
+              mode = "auto";
+
+              templates = {
+                enable_builtin_templates = true;
+              };
             };
-          };
 
-          location = {
-            auto_locate = false;
-            custom_schedule = true;
-            sunrise = "09:00";
-            sunset = "18:00";
+            wallpaper = rec {
+              enabled = true;
+              # (note that `directory` only seems to really be for the automation/randomizer)
+              directory = "${config.xdg.configHome}/${wallpapersCfgPath}";
+              default.path = "${directory}/whitepine.png";
+              fill_mode = "fit";
+              transition = [ "fade" ];
+              transition_on_startup = true;
+              transition_duration = 1500;
+
+              automation = {
+                enabled = true;
+                interval_seconds = 1800;
+                order = "random";
+                recursive = false;
+              };
+            };
+
+            location = {
+              auto_locate = false;
+              custom_schedule = true;
+              sunrise = "09:00";
+              sunset = "18:00";
+            };
+
+            shell = {
+              show_location = false;
+              polkit_agent = true;
+            };
           };
         };
       };
-    };
   };
 }
