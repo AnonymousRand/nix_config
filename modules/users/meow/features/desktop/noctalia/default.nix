5bdf184b78ac2c012c224ae487923c4b3b40c4b7
AnonymousRand <anonymouserand@gmail.com>
2026-08-04 01:26:18 -0700

    niri and noctalia tweaks

--- modules/users/meow/features/desktop/noctalia/default.nix
+++ modules/users/meow/features/desktop/noctalia/default.nix
@@ -4,26 +4,18 @@
       den.aspects.features.desktop.noctalia
     ];
 
-    homeManager = { config, ... }:
-      let
-        wallpapersCfgPath = "noctalia/wallpapers";
-      in
-      {
-        # (note: can't user `config.toml` dotfile as `settings` also set in noctalia theming aspect,
-        # and `settings` seems to completely set the entire `config.toml` file)
-        xdg.configFile."noctalia/config2.toml".source = ./dotfiles/config.toml;
+    homeManager = { config, ... }: {
+      xdg.configFile."noctalia" = {
+        source = ./dotfiles;
+        recursive = true;
+      };
 
-        xdg.configFile."${wallpapersCfgPath}" = {
-          source = ./dotfiles/wallpapers;
-          recursive = true;
-        };
-
-        programs.noctalia = {
-          settings = {
-            # (note that `directory` only seems to be for automation/randomizer, else set `default.path`)
-            wallpaper.directory = "${config.xdg.configHome}/${wallpapersCfgPath}";
-          };
+      programs.noctalia = {
+        settings = {
+          # (note that `directory` only seems to be for automation/randomizer, else set `default.path`)
+          wallpaper.directory = "${config.xdg.configHome}/wallpapers/";
         };
       };
+    };
   };
 }
