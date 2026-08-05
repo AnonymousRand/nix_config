0551233590eb04777f65308a77ba8366364faf86
AnonymousRand <anonymouserand@gmail.com>
2026-08-04 23:26:52 -0700

    okay

--- modules/users/meow/theme/gtk.nix
+++ modules/users/meow/theme/gtk.nix
@@ -5,8 +5,18 @@
     ];
 
     homeManager = { config, ... }: {
-      theme.gtk.gtk4Css = builtins.readFile
-        "${config.theme.compile-scss.cssOutput}/base_scss/gtk/index.css";
+      # noctalia theming (not using `gtk` aspect's options to accommodate noctalia theming)
+      theme.noctalia-theming.templates = {
+        #gtk3 = {
+        #  input_path = "${config.theme.compile-scss.cssOutput}/base_scss/gtk3/index.css";
+        #  output_path = "$XDG_CONFIG_HOME/gtk-3.0/gtk.css";
+        #};
+
+        gtk4 = {
+          input_path = "${config.theme.compile-scss.cssOutput}/base_scss/gtk4/index.css";
+          output_path = "$XDG_CONFIG_HOME/gtk-4.0/gtk.css";
+        };
+      };
     };
   };
 }
