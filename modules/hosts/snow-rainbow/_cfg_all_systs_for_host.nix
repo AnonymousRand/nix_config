adecfe3780ce881d23b5918579e2c5e005291d88
AnonymousRand <anonymouserand@gmail.com>
2026-08-19 02:36:20 -0700

    tweaksg

--- modules/hosts/snow-rainbow/_cfg_all_systs_for_host.nix
+++ modules/hosts/snow-rainbow/_cfg_all_systs_for_host.nix
@@ -2,6 +2,7 @@
 
 # use this to apply `syst` entity config (e.g. `systSettings`) to all instances of this host's
 # `syst` entities, i.e. its host + ALL homes on it!
+# (although currently somewhat limited in functionality, e.g. when it comes to parametric stuff)
 
 cfg: {
   den.hosts.x86_64-linux.snow-rainbow = cfg;
--- modules/users/meow/_cfg_all_profiles_for_user.nix
+++ modules/users/meow/_cfg_all_profiles_for_user.nix
@@ -2,6 +2,7 @@
 
 # use this to apply `profile` entity config (e.g. `profileSettings`) to all instances of this user's
 # `profile` entities, i.e. all instances of this user on ALL hosts and homes!
+# (although currently somewhat limited in functionality, e.g. when it comes to parametric stuff)
 
 # yes, i know this is incredibly stupid, but since this has to be imported at the very top level
 # of a file, we can't access `den` here without infinite recursion. this means we can't dynamically
