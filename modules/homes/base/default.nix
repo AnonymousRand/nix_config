dddcf7efb3efe4e62b0370789b687e878ff0d090
AnonymousRand <anonymouserand@gmail.com>
2026-09-25 23:15:12 -0700

    hmmmmm.

--- modules/homes/base/default.nix
+++ modules/homes/base/default.nix
@@ -1,8 +1,8 @@
 { den, ... }: {
   # (this *does* work with homes, presumably since there is no cross-entity stuff happening here)
-  den.schema.home.includes = [ den.aspects.homes.base ];
+  den.schema.home.includes = [ den.aspects.user-host.base ];
 
-  den.aspects.homes.base = {
+  den.aspects.user-host.base = {
     includes = [
       # activate the "batteries" associated with these settings
       den.aspects.syst-settings.capabilities
