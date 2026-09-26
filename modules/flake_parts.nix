5c23ecbb13a9155a9b85661430efc8e914eb2b39
AnonymousRand <anonymouserand@gmail.com>
2026-09-26 02:05:38 -0700

    little twaek

--- modules/flake_parts.nix
+++ modules/flake_parts.nix
@@ -6,9 +6,10 @@
     };
   };
 
-  # systems to feed to flake-parts' `perSystem`
-  systems = [
-    "x86_64-linux"
-    "aarch64-linux"
-  ];
+  # note that the systems to feed to flake-parts' `perSystem` i think is set as `den.systems`
+  # when using den; and this should by default collect all the systems of all hosts & homes
+  #systems = [
+  #  "x86_64-linux"
+  #  "aarch64-linux"
+  #];
 }
